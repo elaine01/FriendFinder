6313c12fbb71b78383b9845ad77e01a1641815ac
elaine01 <ecumming.g@gmail.com>
2018-02-11 21:25:13 -0800

    friends.js: added mock data, routing: added api and html routes

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friendsArray = [
+	{
+		name: "Jane",
+		photo: "https://www.ocf.berkeley.edu/~dblab/blog/wp-content/uploads/2012/01/icon-profile.png",
+		scores: [
+			"1",
+			"3",
+			"5",
+			"3",
+			"4",
+			"2",
+			"1",
+			"4",
+			"3",
+			"5"
+		]
+	},
+	{
+		name: "Sam",
+		photo: "https://raw.githubusercontent.com/Infernus101/ProfileUI/0690f5e61a9f7af02c30342d4d6414a630de47fc/icon.png",
+		scores: [
+			"3",
+			"1",
+			"4",
+			"3",
+			"5",
+			"4",
+			"3",
+			"1",
+			"2",
+			"5"
+		]
+	}
+];
+
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+ 
+var app = express();
+var PORT = process.env.PORT || 8080;
+ 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+require('./app/routing/htmlRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on Port: " + PORT);
+})
