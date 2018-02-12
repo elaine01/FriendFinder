5d42d3c484463966b8b669808c25fa1d1e6435ce
elaine01 <ecumming.g@gmail.com>
2018-02-11 23:50:43 -0800

    survey.html: getting user input

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,10 +2,12 @@ var friendsData = require('../data/friends.js');
 
 module.exports = function(app) {
 	app.get('/api/friends', function(req, res) {
+		// diplay JSON of all possible friends
 		res.json(friendsData);
 	});
 
 	app.post('/api/friends', function(req, res) {
+		// handle incoming survey results
 		friendsData.push(req.body);
 	});
 }
